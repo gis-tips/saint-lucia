0c269367362cf77870a953b6d29400dbeefee529
gis-tips <chirigakuchousa@gmail.com>
2025-01-17 11:43:57 -0400

    Add files via upload
    デザイン修正

--- main.js
+++ main.js
@@ -1,37 +1,35 @@
+
 var map = new maplibregl.Map({
   container: 'map',
-  style: 'https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json', // 地図のスタイル
-  center: [-60.9897593, 14.0110434], // 中心座標
-  zoom: 16, // ズームレベル
+  style: 'https://tile.openstreetmap.jp/styles/osm-bright-ja/style.json',
+  center: [-60.9897593, 14.0110434],
+  zoom: 16,
 });
 
-// devicePixelRatio を考慮したスケーリングの設定（変更箇所）
 let pixelRatio = window.devicePixelRatio || 1; 
 if (pixelRatio > 1) { 
-  map.setPixelRatio(1.5); // Retinaディスプレイ用にスケール調整
+  map.setPixelRatio(3); 
 } else {
-  map.setPixelRatio(1); // 通常のディスプレイ用
+  map.setPixelRatio(1); 
 }
 
-
-// スケールコントロールを追加 
 var scale = new maplibregl.ScaleControl({
-  maxWidth: 160, // スケールの幅 
-  unit: 'metric' // メートル表記 
+  maxWidth: 160,
+  unit: 'metric'
 }); 
-map.addControl(scale, 'bottom-right'); // 右下に配置 
+map.addControl(scale, 'bottom-right'); 
 
-// 方位記号を追加 
-var nav = new maplibregl.NavigationControl({ 
-  visualizePitch: true // ピッチ（傾き）の表示を有効化 
+var nav = new maplibregl.NavigationControl({
+  visualizePitch: true,
+  zoomInText: '+',
+  zoomOutText: '-'
 }); 
-map.addControl(nav, 'top-left'); // 左上に配置  
+map.addControl(nav, 'top-left');
 
-// 現在地を取得し、地図の中心に設定
 if (navigator.geolocation) { 
   navigator.geolocation.getCurrentPosition(function(position) {
     var userLocation = [position.coords.longitude, position.coords.latitude]; 
-    map.setCenter(userLocation); // 地図の中心座標を現在地に設定 
+    map.setCenter(userLocation); 
   }, function(error) { 
     console.error("現在地の取得に失敗しました。", error); 
   }); 
@@ -40,23 +38,20 @@ if (navigator.geolocation) {
 }
 
 var popup = new maplibregl.Popup({
-  offset: 25, // ポップアップの位置
-  closeButton: false, // 閉じるボタンの表示
+  offset: 25,
+  closeButton: false,
 }).setHTML('<div style="font-size: 1.5em;">George F. L. Charles Airport</div>');
 
 var marker = new maplibregl.Marker().setLngLat([-60.9946090, 14.0203937]).setPopup(popup).addTo(map);
 
 map.on('load', async () => {
-  // 使用するアイコンのIDをリストに追加
   const iconIDs = [
     '1A', '1B', '1D', '1E', '1F', '2A', '2B',
     '2C', '2H', '3A', '3B', '3C', '3D', '3E',
     '3F', '5A', '5B', '5C', '5D', '5E', '5F'
   ];
 
-  // 各アイコンをロードし、地図に追加
   for (let iconID of iconIDs) {
-    // 画像を非同期にロード
     await loadImage(`./img/${iconID}.png`).then((image) => {
       map.addImage(iconID, image);
     }).catch((error) => {
@@ -64,19 +59,17 @@ map.on('load', async () => {
     });
   }
 
-  // GeoJSONデータをソースとして追加
   map.addSource('facility_point', {
     type: 'geojson',
     data: './data/point.geojson',
   });
 
-  // ポイントレイヤーを追加し、各ポイントにアイコンを設定
   map.addLayer({
     id: 'facility_point',
     type: 'symbol',
     source: 'facility_point',
     layout: {
-      'icon-image': ['get', 'ID'],  // 各ポイントのIDフィールドをアイコン名として使用
+      'icon-image': ['get', 'ID'],
       'icon-size': 0.1
     }
   });
@@ -84,7 +77,6 @@ map.on('load', async () => {
   console.log('GeoJSONデータのロードに成功しました');
 });
 
-// 画像をロードする関数
 function loadImage(url) {
   return new Promise((resolve, reject) => {
     const img = new Image();
@@ -94,24 +86,20 @@ function loadImage(url) {
   });
 }
 
-// 地物クリック時にポップアップを表示する
 map.on('click', 'facility_point', (e) => {
   var coordinates = e.features[0].geometry.coordinates.slice();
   var name = e.features[0].properties.Name;
-  var id = e.features[0].properties.ID; // IDプロパティを取得
 
   while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
     coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
   }
 
-  // ポップアップの内容に画像を追加
   var popupContent = `<div style="font-size: 1.5em;"><strong></strong> ${name} Bus Stop<br>
-                      <img src="./tagedphoto/${name}.jpg" alt="${name}" style="width:200px;height:auto;"></div>`;
+                      <img src="./tagedphoto/${name}.jpg" alt="${name}" style="width:400px;height:auto;"></div>`;
 
-  // ポップアップを表示する
   new maplibregl.Popup({
-    offset: 10, // ポップアップの位置
-    closeButton: false, // 閉じるボタンの表示
+    offset: 10,
+    closeButton: false,
   })
     .setLngLat(coordinates)
     .setHTML(popupContent)
@@ -124,7 +112,6 @@ map.on('load', () => {
     data: './data/line.geojson',
   });
 
-  // 通常のラインレイヤー
   map.addLayer({
     id: 'castries_bus',
     type: 'line',
@@ -137,19 +124,16 @@ map.on('load', () => {
       'line-color': [ 
         'match', 
         ['get', 'Name'], 
-        // 条件に基づいて色を指定 
-        ['1A', '1B', '1D', '1E', '1F'], '#dcbf1b', // 黄色 
-        ['2A', '2B', '2C', '2H'], '#08b584', // 緑 
-        ['3A', '3B', '3C', '3D', '3E', '3F'], '#ff601a', // オレンジ 
-        ['5A', '5B', '5C', '5D', '5E', '5F'], '#c83939', // 赤 
-        /* デフォルトの色 */ 
+        ['1A', '1B', '1D', '1E', '1F'], '#dcbf1b',
+        ['2A', '2B', '2C', '2H'], '#08b584',
+        ['3A', '3B', '3C', '3D', '3E', '3F'], '#ff601a',
+        ['5A', '5B', '5C', '5D', '5E', '5F'], '#c83939',
         '#0067c0'
-      ], 
-      'line-width': 5,
+      ],
+      'line-width': 8,
     },
   });
 
-  // ハイライト用のラインレイヤー
   map.addLayer({
     id: 'castries_bus_highlight',
     type: 'line',
@@ -159,25 +143,22 @@ map.on('load', () => {
       'line-cap': 'round',
     },
     paint: {
-      'line-color': '#ffff00', // ホバー時の黄色
+      'line-color': '#ffff00',
       'line-width': 7,
     },
-    filter: ['==', 'Name', ''], // 初期状態ではフィルタが適用されており、何も表示しない
+    filter: ['==', 'Name', ''],
   });
 
-  // マウスオーバー時にハイライト
   map.on('mousemove', 'castries_bus', (e) => {
     if (e.features.length > 0) {
       map.setFilter('castries_bus_highlight', ['==', 'Name', e.features[0].properties.Name]);
     }
   });
 
-  // マウスが外れた時にハイライトを解除
   map.on('mouseleave', 'castries_bus', () => {
     map.setFilter('castries_bus_highlight', ['==', 'Name', '']);
   });
 
-  // クリックイベントでポップアップを表示
   map.on('click', 'castries_bus', (e) => {
     if (e.features.length > 0) {
       const feature = e.features[0];
@@ -185,11 +166,9 @@ map.on('load', () => {
       const routeNumber = feature.properties.Name;
       const destination = feature.properties.description;
 
-      // ポップアップの内容を設定
       const popupContent = `<div style="font-size: 1.5em;"><strong>Route number:</strong> ${routeNumber}<br>
                             <strong>Destination:</strong> ${destination}</div>`;
 
-      // ポップアップを表示
       new maplibregl.Popup()
         .setLngLat(coordinates)
         .setHTML(popupContent)
